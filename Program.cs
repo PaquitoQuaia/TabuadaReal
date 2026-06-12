4910cb8a3ff16894a22c849c5c8e189d6c5f44a4
Quaia <pedro.quaia@edu.senai.br>
2026-06-12 09:19:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class TabuadaExibir
+    {
+        static void ExibirTabuada(int numero)
+        {
+            int contador = 1;
+            while (contador <= 10)
+            {
+                Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+                contador++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para exibir a tabuada: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            ExibirTabuada(numero);
+        }
+    }
+}
